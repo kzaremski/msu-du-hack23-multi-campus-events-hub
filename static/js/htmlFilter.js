dadb51a8e4cfaf74072986e5350340be7294086a
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2023-10-22 13:26:40 -0600

    Filter changes

--- static/js/htmlFilter.js
+++ static/js/htmlFilter.js
@@ -4,7 +4,10 @@ function htmlFilter(myString) {
 	let descrFiltered = myString.replaceAll("&nbsf", "");
 	descrFiltered = descrFiltered.replaceAll("&NBSF", "");
 	descrFiltered = descrFiltered.replaceAll("&AMP", "");
+	descrFiltered = descrFiltered.replaceAll("&AMP;", "");
 	descrFiltered = descrFiltered.replaceAll("&amp", "");
+	descrFiltered = descrFiltered.replaceAll("&amp;", "");
+	descrFiltered = descrFiltered.replaceAll(";amp;", "");
 
 	let array = descrFiltered.split("<");
 	descrFiltered = []; //empties descrFiltered so that it can be filled with the filtered str later
@@ -12,10 +15,10 @@ function htmlFilter(myString) {
 
 	//For loop stores concatenates the filtered strings into descrFiltered
 	for (let i = 0; i < array.length; i++) {
-	//  if (i !== 0) {
+	  if (i !== 0) {
 	  	tempArray = array[i].split(">");
 	  	array[i] = tempArray[1]
-	  //}
+	  }
 	  descrFiltered += array[i];
 	}
 	return descrFiltered;
@@ -23,7 +26,7 @@ function htmlFilter(myString) {
 
 
 //grabs all items that need to be filtered
-let toFilter = document.querySelectorAll(".list-group-item .needs-html-filter");
+let toFilter = document.querySelectorAll(".needs-html-filter");
 //changes the html for the text that needs to be filtered
 for(let p = 0; p < toFilter.length; p++) {
 	toFilter[p].innerHTML = htmlFilter(toFilter[p].innerHTML);
