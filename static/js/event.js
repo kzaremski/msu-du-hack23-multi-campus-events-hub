a7ce65a02f94424759c08df42d15fa7f470b45c6
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2023-10-22 14:01:45 -0600

    Display user login history in their settings page

--- static/js/event.js
+++ static/js/event.js
@@ -2,6 +2,6 @@ window.addEventListener("load", () => {
     const box = document.getElementById("sharelink");
     box.value = window.location.href;
     box.addEventListener("click", () => {
-        this.setSelectionRange(0, this.value.length)
+        this.setSelectionRange(0, this.value.length);
     });
 });
